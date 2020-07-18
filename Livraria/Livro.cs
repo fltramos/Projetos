12c40df2dff7c7213c212c76012141787149ed35
fltramos <fltramos@gmail.com>
2020-07-18 00:21:20 -0300

    Projeto iniciado

---
+++ Livraria/Livro.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Livraria
+{
+    class Livro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; private set; }
+        public string Edicao { get; private set; }
+        public string Editora { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; private set; }
+        public double Estoque { get; private set; }
+        
+        public Livro()
+        {
+
+        }
+        public Livro(string titulo, string autor, string edicao, string editora, double preco, int quantidade, double estoque)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            Edicao = edicao;
+            Editora = editora;
+            Preco = preco;
+            Quantidade = quantidade;
+            Estoque = estoque;           
+
+        }
+
+        public Livro(string titulo, double preco, int quantidade)
+        {
+            Titulo = titulo;
+            Preco = preco;
+            Quantidade = quantidade;
+                        
+        }
+
+        public void ValorEstoque()
+        {
+           Estoque = Preco * Quantidade;
+             
+        }
+
+        public void Venda(int quant)
+        {
+            Quantidade -= quant;
+        }
+
+        public void RepoEstoque(int quant)
+        {
+            Quantidade += quant;
+        }
+
+        public override string ToString()
+        {
+            return "Livros cadastrados: " + Titulo
+                + ", "
+                + Preco.ToString("F2")
+                + ", "
+                + Quantidade
+                + " , Valor do Estoque: "
+                + Estoque.ToString("F2");
+        }
+    }
+}
---
+++ Livraria/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Livraria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro livro;
+            
+            Console.WriteLine("Cadastrar Livros no Estoque");
+            Console.Write("Cadastrar Titulo: ");
+            string titulo = Console.ReadLine();
+            Console.Write("Cadastrar Autor: ");
+            string autor = Console.ReadLine();
+            Console.Write("Cadastrar Edição: ");
+            string edicao = Console.ReadLine();
+            Console.Write("Cadastrar Editora: ");
+            string editora = Console.ReadLine();
+            Console.Write("Cadastrar Preço: ");
+            double preco = double.Parse(Console.ReadLine());
+            Console.Write("Cadastrar a Quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            
+            livro = new Livro(titulo, preco, quantidade);
+            livro.ValorEstoque();
+            Console.WriteLine();
+            Console.WriteLine(livro);
+
+            Console.WriteLine();
+
+            Console.Write("Vender Livros: ");
+            int quant = int.Parse(Console.ReadLine());
+            livro.Venda(quant);
+            livro.ValorEstoque();
+            Console.WriteLine(livro);
+
+            Console.WriteLine();
+
+            Console.Write("Entrada no Estoque: ");
+            quant = int.Parse(Console.ReadLine());
+            livro.RepoEstoque(quant);
+            livro.ValorEstoque();
+            Console.WriteLine(livro);
+        }
+    }
+}
